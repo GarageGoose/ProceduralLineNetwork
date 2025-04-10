a446389061ba22e5f7014fa6c35a3c22b003c864
GarageGoose <garagegoosegames@gmail.com>
2025-04-09 20:50:21 -0700

    Fixed stuff

---
+++ ProceduralLineNetworkGen2/CoreComponents/LineNetworkModification/MergePointsInCloseProximity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralLineNetwork.CoreComponents.LineNetworkModification
+{
+    internal class MergePointsInCloseProximity
+    {
+    }
+}
